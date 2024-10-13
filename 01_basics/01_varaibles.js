94b849a5dcfdca5a590d0a09910da21e33892a0a
Naveed Sultan <naveeddar1409@gmail.com>
2024-10-13 05:35:09 +0000

    varaibles

---
+++ 01_basics/01_varaibles.js
@@ -0,0 +1,20 @@
+const accountId = 14453
+let accountEmail = "nsd14@gmal.com"
+var accountPassword = "666668"
+accountCity = "Srinagar"
+let accountState;
+
+//accountId = 2 not allowed
+
+/*accountEmail = "dfhsdkf@ga.com"
+accountPassword = "5234545435"
+accountCity = "Jammu"*/
+
+//console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+/*
+Prefer not to use Var 
+because of Block Scope and Funtional Scope
+*/
