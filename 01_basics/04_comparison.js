6c122d13b30213eb210293949fea033e33b8dcd8
Naveed Sultan <naveeddar1409@gmail.com>
2024-10-16 15:10:10 +0000

    Comparison done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// Here it checks only values
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 == 1);
+//console.log(2 != 1);
+
+//here is also chances of getting problems cause of different dataTypes but still it checks only values
+//console.log("2" > 1);
+//console.log("02" > 1);
+
+//Try to avoid these kind of comparison as they create confusion 
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
+//
+//console.log(undefined > 0);
+//console.log(undefined == 0);
+//console.log(undefined >= 0);
+//console.log(undefined < 0);
+
+//===(Triple Equal) here it will check values also dataTypes
+
+console.log("2" === 2);
