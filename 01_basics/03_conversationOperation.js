813d86c2c9b3ae6ac3d291eae73b855ee3aff0e5
Naveed Sultan <naveeddar1409@gmail.com>
2024-10-16 08:24:10 +0000

    operations

--- 01_basics/03_conversationOperation.js
+++ 01_basics/03_conversationOperation.js
@@ -1,9 +1,9 @@
- let score = "Naveed"
+// let score = "Naveed"
 
 // console.log(typeof score);
 // console.log(typeof(score));
 
-let valueInNumber = Number(score)
+//let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber)
 
@@ -13,17 +13,56 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 // true => 1 false => 0
 
-let isLoggedIn = "Naveed"
+//let isLoggedIn = "Naveed"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "hitesh" => true
 
-let someNumber = 33
+//let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+//let stringNumber = String(someNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber);
+
+//*************************************Operations*************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);//two raise to power 2
+//console.log(2/2);
+//console.log(2%2);//remainder
+
+let str1 = "Naveed"
+let str2 = " Sultan"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2 );
+console.log(1 + "2")
+console.log("1" + 2 + 3)
+console.log(1 + 2 + "3")
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100 
+//gameCounter++; //postfix (using increment with operator after operand)
+++gameCounter; //prefix ( using increment with operator before operand)
+console.log(gameCounter);
